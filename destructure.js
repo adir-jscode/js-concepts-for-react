a1940f6caebbecaa2220d5d66b1e0d9352b6b4c1
adir-jscode <adir.jscode@gmail.com>
2022-03-11 23:29:25 +0600

    fixed

--- destructure.js
+++ destructure.js
@@ -22,8 +22,8 @@ const student = {
 const [firstMovie, secondMovie] = student.movies;
 
 // object destructuring
-const { name, age } = { name: 'alu', age: 14 };
-const { name, age,salary } = { id: 12, name: 'alu', salary: 3400, age: 14 };
+// const { name, age } = { name: 'alu', age: 14 };
+// const { name, age,salary } = { id: 12, name: 'alu', salary: 3400, age: 14 };
 
 const employee = {
     ide: 'VS Code',
